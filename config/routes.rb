b99966719798cf696cfc59d9de41a59eb2caae35
leah <donstott@ggc.edu>
2017-09-19 17:41:36 +0000

    Add Hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
-Rails.application.routes.draw do
+#Rails.application.routes.draw do
+#  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+Rails.application.routes.draw do 
+  root 'application#hello' 
 end
